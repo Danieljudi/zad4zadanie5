e46bc804ef550c2ccfd19c5007bc5b1c255888fb
Danieljudi <41111907+Danieljudi@users.noreply.github.com>
2018-09-28 20:28:27 +0200

    usuniete scannery i boczne scianki

--- src/Main.java
+++ src/Main.java
@@ -9,20 +9,17 @@ public class Main {
         String imie= scanner.nextLine();
 
         System.out.println("Podaj nazwisko:");
-        Scanner scanner2=new Scanner(System.in);
         String nazwisko=scanner.nextLine();
 
         System.out.println("Podaj nr telefonu:");
-        Scanner scanner3=new Scanner(System.in);
         String numer=scanner.nextLine();
 
 
         System.out.println("================================");
         System.out.println("|                              |");
-        System.out.println("|   "+imie+"                   |");//
-        System.out.println("|   "+nazwisko+"               |");// poprawić pozycjonowanie prawej krawedzi
-        System.out.println("|   "+numer+"                  |");//
-        System.out.println("|                              |");
+        System.out.println("   "+imie+"                   ");
+        System.out.println("   "+nazwisko+"               ");
+        System.out.println("   "+numer+"                  ");
         System.out.println("|                              |");
         System.out.println("================================");
 
